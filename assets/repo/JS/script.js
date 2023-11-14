e0adb42bf259431ef9ae486aa7fa47ef1d8c0a68
appletech75 <xbalaguer2012@gmail.com>
2023-11-14 14:16:12 +0100

    Made A new Site Again bc the old one was not working well on old ios

--- assets/repo/JS/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const menuIcon = document.querySelector('.menu-icon');
-  const mobileNav = document.querySelector('.mobile-nav');
-
-  menuIcon.addEventListener('click', function () {
-      mobileNav.classList.toggle('active');
-  });
-});
